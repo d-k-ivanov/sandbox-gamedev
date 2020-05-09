3fc9e9594b22a1e8a93f312e63f6e9ebe247c186
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-05-09 11:49:48 +0300

    switch to 320x180 and to move and slide

--- godot-2d-action-rpg/Source/Player/Player.cs
+++ godot-2d-action-rpg/Source/Player/Player.cs
@@ -3,11 +3,11 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    [Export] public int MaxSpeed          = 160;
-    [Export] public int Acceleration      = 1000;
-    private const   int BreakingFraction  = 1000;
+    [Export] public int MaxSpeed            = 160;
+    [Export] public int Acceleration        = 1000;
+    private  const  int BreakingFraction    = 1000;
 
-    private Vector2     _velocity      = Vector2.Zero;
+    private Vector2 _velocity = Vector2.Zero;
 
     public override void _Ready()
     {
@@ -30,8 +30,8 @@ public class Player : KinematicBody2D
             _velocity = _velocity.MoveToward(Vector2.Zero, BreakingFraction * delta) ;
         }
 
-        GD.Print(_velocity);
+        // GD.Print(_velocity);
 
-        MoveAndCollide( _velocity * delta);
+        MoveAndSlide( _velocity);
     }
 }
