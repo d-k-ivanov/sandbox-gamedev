19541b22a35793defd3b70bf021b947fbb546133
Dmitry Ivanov <d.k.ivanov@live.com>
2022-05-30 13:28:04 +0200

    COMP4300: refactor gen.ps1

--- comp4300_game_programming/gen.ps1
+++ comp4300_game_programming/gen.ps1
@@ -1,22 +1,23 @@
+param
+(
+    [switch] $Conan
+)
 
-if (Get-Command conan -ErrorAction SilentlyContinue | Test-Path)
+if ($Conan)
 {
+    if (-Not (Get-Command conan -ErrorAction SilentlyContinue | Test-Path))
+    {
+        Write-Host "Conan not found. Please install it first. Exiting..." -ForegroundColor Red
+        exit 1
+    }
     conan install "${PSScriptRoot}" -pr "${Env:USERPROFILE}\.conan\profiles\windows-msvc-17-static-debug-x64"          --build=missing -if ./_conan/debug
     conan install "${PSScriptRoot}" -pr "${Env:USERPROFILE}\.conan\profiles\windows-msvc-17-static-release-x64"        --build=missing -if ./_conan/release
     conan install "${PSScriptRoot}" -pr "${Env:USERPROFILE}\.conan\profiles\windows-msvc-17-static-relwithdebinfo-x64" --build=missing -if ./_conan/relwithdebinfo
 }
-else
-{
-    Write-Host "Conan not found. Please install it first. Exiting..." -ForegroundColor Red
-    exit 1
-}
 
-if (Get-Command premake5 -ErrorAction SilentlyContinue | Test-Path)
-{
-    premake5 vs2022
-}
-else
+if (-Not (Get-Command premake5 -ErrorAction SilentlyContinue | Test-Path))
 {
     Write-Host "Premake not found. Please install it first. Exiting..."  -ForegroundColor Red
     exit 1
 }
+premake5 vs2022
