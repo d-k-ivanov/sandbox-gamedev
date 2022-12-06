24a91bb24dfea2792e9326a354057c60095c3283
Dmitry Ivanov <d.k.ivanov@live.com>
2022-12-07 00:26:34 +0100

    comp4300_game_programming: playing with SFML shape arrays

--- comp4300_game_programming/SFMLBasicShapes2D/main.cpp
+++ comp4300_game_programming/SFMLBasicShapes2D/main.cpp
@@ -121,6 +121,89 @@ int main(int argc, char* argv[])
     triangles[7].color = sf::Color(255, 255, 0);
     triangles[8].color = sf::Color(255, 255, 0);
 
+    // stripped triangles
+    sf::VertexArray trianglesStripped(sf::TriangleStrip, 10);
+    trianglesStripped[0].position = sf::Vector2f(100, 200);
+    trianglesStripped[1].position = sf::Vector2f(100, 400);
+    trianglesStripped[2].position = sf::Vector2f(300, 200);
+    trianglesStripped[0].color = sf::Color(0, 181, 99);
+    trianglesStripped[1].color = sf::Color(0, 181, 99);
+    trianglesStripped[2].color = sf::Color(102, 0, 99);
+
+    trianglesStripped[3].position = sf::Vector2f(300, 400);
+    trianglesStripped[4].position = sf::Vector2f(500, 300);
+    trianglesStripped[5].position = sf::Vector2f(500, 500);
+    trianglesStripped[3].color = sf::Color(102, 0, 99);
+    trianglesStripped[4].color = sf::Color(255, 255, 0);
+    trianglesStripped[5].color = sf::Color(255, 255, 0);
+
+    trianglesStripped[6].position = sf::Vector2f(700, 300);
+    trianglesStripped[7].position = sf::Vector2f(700, 500);
+    trianglesStripped[8].position = sf::Vector2f(900, 400);
+    trianglesStripped[9].position = sf::Vector2f(900, 600);
+    trianglesStripped[6].color = sf::Color(126, 221, 54);
+    trianglesStripped[7].color = sf::Color(126, 221, 54);
+    trianglesStripped[8].color = sf::Color(0, 0, 205);
+    trianglesStripped[9].color = sf::Color(0, 0, 205);
+
+    // triangles with the one same vertex
+    sf::VertexArray triangleFan(sf::TriangleFan, 12);
+    triangleFan[0].position = sf::Vector2f(250, 200);
+    triangleFan[1].position = sf::Vector2f(350, 250);
+    triangleFan[2].position = sf::Vector2f(300, 300);
+    triangleFan[0].color = sf::Color::Yellow;
+    triangleFan[1].color = sf::Color::White;
+    triangleFan[2].color = sf::Color::White;
+
+    triangleFan[3].position = sf::Vector2f(250, 315);
+    triangleFan[4].position = sf::Vector2f(200, 300);
+    triangleFan[5].position = sf::Vector2f(140, 250);
+    triangleFan[3].color = sf::Color::Red;
+    triangleFan[4].color = sf::Color::Red;
+    triangleFan[5].color = sf::Color::Magenta;
+
+    triangleFan[6].position = sf::Vector2f(130, 200);
+    triangleFan[7].position = sf::Vector2f(140, 150);
+    triangleFan[8].position = sf::Vector2f(200, 100);
+    triangleFan[9].position = sf::Vector2f(250, 80);
+    triangleFan[10].position = sf::Vector2f(300, 100);
+    triangleFan[11].position = sf::Vector2f(350, 150);
+    triangleFan[6].color = sf::Color::Magenta;
+    triangleFan[7].color = sf::Color::Green;
+    triangleFan[8].color = sf::Color::Green;
+    triangleFan[9].color = sf::Color::Cyan;
+    triangleFan[10].color = sf::Color::Cyan;
+    triangleFan[11].color = sf::Color::Yellow;
+
+    // quads
+    sf::VertexArray quads(sf::Quads, 12);
+    quads[0].position = sf::Vector2f(400, 300);
+    quads[1].position = sf::Vector2f(400, 200);
+    quads[2].position = sf::Vector2f(500, 200);
+    quads[3].position = sf::Vector2f(500, 300);
+    quads[0].color = sf::Color(255, 102, 255);
+    quads[1].color = sf::Color(255, 102, 255);
+    quads[2].color = sf::Color(255, 102, 255);
+    quads[3].color = sf::Color(255, 102, 255);
+
+    quads[4].position = sf::Vector2f(600, 300);
+    quads[5].position = sf::Vector2f(600, 200);
+    quads[6].position = sf::Vector2f(800, 200);
+    quads[7].position = sf::Vector2f(800, 300);
+    quads[4].color = sf::Color(102, 0, 99);
+    quads[5].color = sf::Color(102, 0, 99);
+    quads[6].color = sf::Color(102, 0, 99);
+    quads[7].color = sf::Color(102, 0, 99);
+
+    quads[8].position = sf::Vector2f(500, 500);
+    quads[9].position = sf::Vector2f(600, 400);
+    quads[10].position = sf::Vector2f(800, 400);
+    quads[11].position = sf::Vector2f(700, 500);
+    quads[8].color = sf::Color(255, 88, 0);
+    quads[9].color = sf::Color(255, 88, 0);
+    quads[10].color = sf::Color(255, 88, 0);
+    quads[11].color = sf::Color(255, 88, 0);
+
     while (window.isOpen())
     {
         sf::Event event{};
@@ -136,7 +219,10 @@ int main(int argc, char* argv[])
         // window.draw(points);
         // window.draw(lines);
         // window.draw(stripLines);
-        window.draw(triangles);
+        // window.draw(triangles);
+        // window.draw(trianglesStripped);
+        // window.draw(triangleFan);
+        window.draw(quads);
         window.display();
     }
 
